2c6aa0fef8bf87927108770f0e01a45abbceada5
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-09-16 17:36:00 -0400

    landing page displays a list of posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,45 @@ const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Posts</h1>
+    <ul>
+      {data.allWpPost.nodes.map(post => (
+        <li>
+          <Link
+            to={`/blog${post.link}`}   
+          >
+            <h2>{post.title}</h2>
+          </Link>
+          <div
+            dangerouslySetInnerHTML={{ __html: post.excerpt }}
+          />
+          <Link
+            to={`/blog${post.link}`}   
+          >
+            <p>Read More</p>
+          </Link>
+
+        </li>
+      ))}
+    </ul>
+
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
 export default IndexPage
+
+export const query = graphql`
+  {
+    allWpPost {
+      nodes {
+        id
+        title
+        excerpt
+        slug
+        link
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`
